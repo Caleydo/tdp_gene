32abdd812cdad0b00237541a62feb50740084fff
Holger Stitz <holger.stitz@jku.at>
2016-12-06 15:55:13 +0100

    Load gene labeles in CommonProxyView Caleydo/targid2#173

--- CommonProxyView.ts
+++ CommonProxyView.ts
@@ -2,9 +2,11 @@
  * Created by Holger Stitz on 06.12.2016.
  */
 
+import ajax = require('../caleydo_core/ajax');
 import {IViewContext, ISelection} from '../targid2/View';
 import {ProxyView} from '../targid2/ProxyView';
 import {IPluginDesc} from '../caleydo_core/plugin';
+import {gene, getSelectedSpecies} from './Common';
 
 /**
  * helper view for proxying an existing external website
@@ -22,9 +24,20 @@ export class CommonProxyView extends ProxyView {
    * @returns {string[]}
    */
   protected filterSelectedNames(names:string[]):string[] {
-      //FIXME HACK for UnitProt
-      //filter 'AO*' UnitPort IDs that are not valid for external canSAR database
-      return names.filter(d => d.indexOf('A0') !== 0);
+    //FIXME HACK for UnitProt
+    //filter 'AO*' UnitPort IDs that are not valid for external canSAR database
+    return names.filter(d => d.indexOf('A0') !== 0);
+  }
+
+  protected getSelectionDropDownLabels(names:string[]):Promise<string[]> {
+    return ajax.getAPIJSON(`/targid/db/${gene.db}/gene_map_ensgs`, {
+        ensgs: `'${names.join('\',\'')}'`,
+        species: getSelectedSpecies()
+      })
+      .then((mapping) => {
+        // resolve ensg to gene name
+        return mapping.map((d, i) => `${d.symbol} (${names[i]})`);
+      });
   }
 
 }
