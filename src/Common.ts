bb4aaa7ff0d59643167956c7583f7ce693d03f20
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-03-06 14:01:29 +0100

    extract constants to own file

--- src/Common.ts
+++ src/Common.ts
@@ -4,23 +4,8 @@
 
 import * as session from 'phovea_core/src/session';
 
-export const copyNumberCat = [
-  {value: 2, name: 'Amplification', color: '#efb3bc', border: 'transparent'},
-  {value: -2, name: 'Deep Deletion', color: '#92c5de', border: 'transparent'},
-  //{value: -1, name: 'Heterozygous deletion', color: '#92c5de'},
-  {value: 0, name: 'NORMAL', color: '#dcdcdc', border: 'transparent'},
-  //{value: 1, name: 'Low level amplification', color: '#f4a582'},
-  //{value: 2, name: 'High level amplification', color: '#ca0020'},
-  {value: 'null', name: 'Unknown', color: '#FCFCFC', border: '#dcdcdc'}
-];
-export const unknownCopyNumberValue: any = copyNumberCat[copyNumberCat.length-1].value;
-
-export const mutationCat = [
-  {value: 'true', name: 'Mutated', color: '#1BA64E', border: 'transparent'},
-  {value: 'false', name: 'Non Mutated', color: '#aaa', border: 'transparent'},
-  {value: 'null', name: 'Unknown', color: 'transparent', border: '#999'}
-];
-export const unknownMutationValue: any = mutationCat[mutationCat.length-1].value;
+export {copyNumberCat, mutationCat, unknownCopyNumberValue, unknownMutationValue} from './constants';
+import {mutationCat, copyNumberCat, unknownMutationValue, unknownCopyNumberValue} from './constants';
 
 export const allTypes = 'All Tumor Types';
 export const allBioTypes = 'All Bio Types';
