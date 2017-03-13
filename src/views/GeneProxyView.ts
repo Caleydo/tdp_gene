93e806707462dbb2b81c7576bf25f86a706d2bce
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-03-13 10:31:02 +0100

    use promises for better debugging

--- src/views/GeneProxyView.ts
+++ src/views/GeneProxyView.ts
@@ -13,18 +13,24 @@ import {IFormSelectOption} from 'ordino/src/FormBuilder';
  */
 export default class GeneProxyView extends ProxyView {
 
-  protected async getSelectionSelectData(ensgs: string[]): Promise<IFormSelectOption[]> {
+  protected getSelectionSelectData(ensgs: string[]): Promise<IFormSelectOption[]> {
     if (ensgs === null || ensgs.length === 0) {
       return Promise.resolve([]);
     }
     const idType = this.selection.idtype;
-    const target = await selectReadableIDType(idType);
-    if (!target) {
-      return ensgs.map((ensg) => ({value: ensg, name: ensg, data: ensg}));
-    }
-    // map and use names
-    const names = await idType.mapToFirstName(this.selection.range, target);
-    return names.map((name, i) => ({value: ensgs[i], name: name ? `${name} (${ensgs[i]})` : ensgs[i], data: ensgs[i]}));
+    return selectReadableIDType(idType).then((target) => {
+      if (!target) {
+        return ensgs.map((ensg) => ({value: ensg, name: ensg, data: ensg}));
+      }
+      // map and use names
+      return idType.mapToFirstName(this.selection.range, target).then((names) => {
+        return names.map((name, i) => ({
+          value: ensgs[i],
+          name: name ? `${name} (${ensgs[i]})` : ensgs[i],
+          data: ensgs[i]
+        }));
+      });
+    });
   }
 }
 
