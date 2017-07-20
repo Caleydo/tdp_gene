a71e993fa5382f84a36a9e2c7f4cd53157fe46ef
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-07-20 14:07:32 +0200

    fix datavisyn/targid_bob#12 gene symbols

--- src/entries/SearchProvider.ts
+++ src/entries/SearchProvider.ts
@@ -17,12 +17,24 @@ export interface ISearchProvider {
 
 export default class SearchProvider implements ISearchProvider {
 
-  constructor(private readonly dataSource: { db: string, base: string, entityName: string }) {
+  constructor(protected readonly dataSource: { db: string, base: string, entityName: string }) {
 
   }
 
+  get searchUrl() {
+    return `/targid/db/${this.dataSource.db}/${this.dataSource.base}_items/lookup`;
+  }
+
+  get verifyUrl() {
+    return `/targid/db/${this.dataSource.db}/${this.dataSource.base}_items_verfiy/filter`;
+  }
+
+  protected mapItems(result: any): IResult {
+    return Object.assign(result, {id: result.targidid, extra: result.id});
+  }
+
   search(query: string, page: number, pageSize: number): Promise<{ more: boolean, results: IResult[] }> {
-    return getAPIJSON(`/targid/db/${this.dataSource.db}/${this.dataSource.base}_items/lookup`, {
+    return getAPIJSON(this.searchUrl, {
       column: this.dataSource.entityName,
       species: getSelectedSpecies(),
       query,
@@ -30,7 +42,7 @@ export default class SearchProvider implements ISearchProvider {
       limit: pageSize
     }).then((data) => {
       return {
-        results: data.items.map((d) => Object.assign(d, {id: d.targidid, extra: d.id})),
+        results: data.items.map(this.mapItems.bind(this)),
         more: data.more
       };
     });
@@ -38,7 +50,10 @@ export default class SearchProvider implements ISearchProvider {
 
 
   validate(query: string[]): Promise<IResult[]> {
-    //TODO
-    return Promise.resolve([]);
+    return getAPIJSON(this.verifyUrl, {
+      column: this.dataSource.entityName,
+      species: getSelectedSpecies(),
+      [`filter_${this.dataSource.entityName}`]: query,
+    }).then((data) => data.map(this.mapItems.bind(this)));
   }
 }
