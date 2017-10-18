6ff68bd1c4b1b06321cccace99e2705ae10ed15d
Christian Lehner <christian.lehner@datavisyn.io>
2017-10-18 16:27:22 +0200

    remove spinners when changing species

--- src/menu/ACommonSubSection.ts
+++ src/menu/ACommonSubSection.ts
@@ -57,7 +57,7 @@ export abstract class ACommonSubSection implements IStartMenuSubSection {
   }
 
   update() {
-    this.data.update();
+    this.data.update(true);
   }
 
   push(namedSet: INamedSet) {
