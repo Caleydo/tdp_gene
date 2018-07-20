0902a30411e26715d89a276c7661e0a0da831166
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-07-20 17:52:01 +0200

    better tags

--- src/phovea.ts
+++ src/phovea.ts
@@ -17,8 +17,8 @@ export default function (registry: IRegistry) {
   });
   /// #endif
 
-  function tdpView(id: string, loader: () => any, desc: any) {
-    registry.push('tdpView', id, loader, desc);
+  function tdpView(id: string, loader: () => any, desc: any, ...descs) {
+    registry.push('tdpView', id, loader, Object.assign(desc, ...descs));
   }
 
   // proxy pages
@@ -34,7 +34,7 @@ export default function (registry: IRegistry) {
       // 'order: 0
     },
     description: 'Show information on your search from Ensembl.org',
-    topics: ['ensembl', 'external']
+    topics: ['general', 'external']
   });
 
   tdpView('cansar', () => System.import('./views/UniProtProxyView'), {
@@ -52,7 +52,7 @@ export default function (registry: IRegistry) {
       species: 'human'
     },
     description: 'Show information on your search from the canSAR page',
-    topics: ['cansar', 'external']
+    topics: ['general', 'external']
   });
 
   tdpView('uniprot', () => System.import('./views/UniProtProxyView'), {
@@ -67,7 +67,7 @@ export default function (registry: IRegistry) {
       // 'order: 70
     },
     description: 'Show information on your search from UniProt',
-    topics: ['uniprot', 'external']
+    topics: ['uniprot', 'general', 'external']
   });
 
   tdpView('targetvalidation', () => System.import('./views/GeneProxyView'), {
@@ -103,7 +103,7 @@ export default function (registry: IRegistry) {
       species: 'human'
     },
     description: 'Show information on your search from the Human Protein Atlas',
-    topics: ['protein-atlas', 'external']
+    topics: ['protein', 'external']
   });
 
   const common = {
@@ -120,7 +120,7 @@ export default function (registry: IRegistry) {
     topics: ['external']
   };
 
-  tdpView('humanproteomemap', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('humanproteomemap', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'Human Proteome Map',
     site: 'http://www.humanproteomemap.org/protein.php?hpm_id={gene}',
     helpUrl: 'http://www.humanproteomemap.org/faqs.html',
@@ -129,68 +129,87 @@ export default function (registry: IRegistry) {
   }, common, {
     idtype: 'EntrezGene',
     readableIDType: 'GeneSymbol',
-  }));
+    topics: ['protein', 'external']
+  });
 
-  tdpView('proteomicsdb', () => System.import('tdp_core/src/views/Chooser/ProxyView'), Object.assign({
+  tdpView('proteomicsdb', () => System.import('tdp_core/src/views/Chooser/ProxyView'), {
     name: 'ProteomicsDB',
     site: 'https://www.proteomicsdb.org/proteomicsdb/#human/search/query?protein_name={gene}',
     argument: 'gene',
     helpUrl: 'https://www.proteomicsdb.org/#faq',
     preview: () => System.import('./assets/previews/proteomicsdb.png'),
     description: 'Proteomics data from proteomicsDB'
-  }, common));
+  }, common, {
+    topics: ['protein', 'external']
+  });
 
+  tdpView('genenames', () => System.import('tdp_core/src/views/ChooserProxyView'), {
+    name: 'Genenames',
+    site: 'https://www.genenames.org/cgi-bin/gene_symbol_report?match={gene}',
+    preview: () => System.import('./assets/previews/genenames.jpg'),
+    description: 'Reference for human gene symbols',
+    helpUrl: 'https://www.genenames.org/about/overview'
+  }, common);
 
-  tdpView('ClinVar', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('ClinVar', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'ClinVar',
     site: 'https://www.ncbi.nlm.nih.gov/clinvar/?term={gene}',
     helpUrl: 'https://www.ncbi.nlm.nih.gov/clinvar/intro/',
     preview: () => System.import('./assets/previews/clinvar.png'),
     description: 'relationships among human variations and phenotypes, with supporting evidence'
-  }, common));
+  }, common);
 
-  tdpView('cosmic_gene', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('cosmic_gene', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'COSMIC',
     site: 'https://cancer.sanger.ac.uk/cosmic/gene/analysis?ln={gene}',
     preview: () => System.import('./assets/previews/cosmic_banner.png'),
     description: 'Catalogue Of Somatic Mutations In Cancer',
     helpUrl: 'https://cancer.sanger.ac.uk/cosmic/about'
-  }, common));
+  }, common, {
+    topics: ['cancer', 'external']
+  });
 
-  tdpView('tumorportal', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('tumorportal', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'Tumor portal',
     site: 'http://www.tumorportal.org/view?geneSymbol={gene}',
     preview: () => System.import('./assets/previews/tumorportal.png'),
     description: 'Tumor portal from the BROAD institute',
     helpUrl: 'https://www.nature.com/articles/nature12912'
-  }, common));
+  }, common, {
+    topics: ['cancer', 'external']
+  });
 
-  tdpView('antibodypedia', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('antibodypedia', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'Antibodypedia',
     site: 'https://www.antibodypedia.com/explore/{gene}',
     preview: () => System.import('./assets/previews/antibodypedia.png'),
     description: 'Searchable database tailored to specific biological and biomedical assays',
     helpUrl: 'https://www.antibodypedia.com/text/about_us'
-  }, common));
+  }, common, {
+    topics: ['compounds', 'external']
+  });
 
-  registry.push('tdpView', 'rndsystems', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('rndsystems', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'R&D Systems',
     site: 'https://www.rndsystems.com/search?keywords={gene}',
     preview: () => System.import('./assets/previews/rndsystems.png'),
     description: 'Tool compound/AB search engine',
     helpUrl: 'https://www.rndsystems.com/about-us'
-  }, common));
+  }, common, {
+    topics: ['compounds', 'external']
+  });
 
-  tdpView('biocompare', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('biocompare', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'Biocompare',
     site: 'https://www.biocompare.com/Search-Antibodies/?search={gene}',
     preview: () => System.import('./assets/previews/biocompare.png'),
     description: 'Find antibodies and more from biocompare.com',
     helpUrl: 'https://www.biocompare.com/About-Biocompare/'
-  }, common));
+  }, common, {
+    topics: ['compounds', 'external']
+  });
 
-
-  tdpView('drugebility', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
+  tdpView('drugebility', () => System.import('tdp_core/src/views/ChooserProxyView'), {
     name: 'DrugEBIlity',
     site: 'https://www.ebi.ac.uk/chembl/drugebility/protein/{protein}',
     preview: () => System.import('./assets/previews/DrugEBIlity.png'),
@@ -199,35 +218,9 @@ export default function (registry: IRegistry) {
   }, common, {
     argument: 'protein',
     idtype: 'Uniprot',
-    readableIDType: 'GeneSymbol'
-  }));
-
-
-  tdpView('genenames', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
-    name: 'Genenames',
-    site: 'https://www.genenames.org/cgi-bin/gene_symbol_report?match={gene}',
-    preview: () => System.import('./assets/previews/genenames.jpg'),
-    description: 'Reference for human gene symbols',
-    helpUrl: 'https://www.genenames.org/about/overview'
-  }, common));
-
-  tdpView('humanproteomemap', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
-    name: 'Human Proteome Map',
-    site: 'http://www.humanproteomemap.org/protein.php?hpm_id={gene}',
-    helpUrl: 'http://www.humanproteomemap.org/faqs.html',
-    preview: () => System.import('./assets/previews/humanproteomemap.png'),
-    description: 'Proteomics data from the human proteome map'
-  }, common, {
-    idtype: 'EntrezGene'
-  }));
-
-  tdpView('proteomicsdb', () => System.import('tdp_core/src/views/ChooserProxyView'), Object.assign({
-    name: 'ProteomicsDB',
-    site: 'https://www.proteomicsdb.org/proteomicsdb/#human/search/query?protein_name={gene}',
-    helpUrl: 'https://www.proteomicsdb.org/#faq',
-    preview: () => System.import('./assets/previews/proteomicsdb.png'),
-    description: 'Proteomics data from proteomicsDB'
-  }, common));
+    readableIDType: 'GeneSymbol',
+    topics: ['protein', 'external']
+  });
 
   registry.push('importPostProcessor', 'GeneSymbol', () => System.import('./common'), {
     factory: 'convertGeneSymbolToEnsembl'
