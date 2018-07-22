068d455d109d05e2e7ac5cfcf9abeb562fc1d20d
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-07-22 11:52:19 +0200

    fix es6 compatibility

--- phovea.js
+++ phovea.js
@@ -9,7 +9,7 @@ module.exports = function (registry) {
   //registry.push('extension-type', 'extension-id', function() { return System.import('./extension_impl'); }, {});
   // generator-phovea:begin
   /// #if include('ordino')
-  registry.push('ordinoStartMenuSection', 'section_species', () => System.import('./src/menu/SpeciesSelectorMenuSection'), {
+  registry.push('ordinoStartMenuSection', 'section_species', function() { return import('./src/menu/SpeciesSelectorMenuSection'); }, {
     name: 'Predefined Datasets',
     cssClass: 'speciesSelector',
     priority: 10
