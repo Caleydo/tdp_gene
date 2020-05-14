d15709b164939089a69e7135680c8419908ce6fd
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 17:48:02 +0200

    add main index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './menu';
+export * from './views';
+
+export * from './common';
+export * from './constants';
+export * from './extensions';
+export * from './forms';
+export * from './GeneIDTypeDetector';
+export {default} from './phovea';
+export * from './SearchProvider';
+export * from './utils';
