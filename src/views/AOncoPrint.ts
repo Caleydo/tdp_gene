aea1c6e134b3b918731c4251b2355bfacd7c4890
oltionchampari <oltion.champari@datavisyn.io>
2020-06-08 12:26:31 +0200

    Fix typo

--- src/views/AOncoPrint.ts
+++ src/views/AOncoPrint.ts
@@ -238,7 +238,7 @@ export abstract class AOncoPrint extends AView {
     copyNumberCat.forEach((d) => {
       $cnLegend.append('li').attr('data-cnv', d.value).text(d.name);
     });
-    // append the legen for missing values
+    // append the legend for missing values
     $cnLegend.append('li').attr('data-cnv', unknownCopyNumberValue).text('Missing Values');
 
     const $mutLegend = $legend.append('ul');
@@ -247,7 +247,7 @@ export abstract class AOncoPrint extends AView {
     mutationCat.forEach((d) => {
       $mutLegend.append('li').attr('data-mut', d.value).text(d.name);
     });
-      // append the legen for missing values
+      // append the legend for missing values
     $mutLegend.append('li').attr('data-mut', unknownMutationValue ).text('Missing Values');
 
     $node.append('div').attr('class', 'alert alert-info alert-dismissible').attr('role', 'alert').html(`
