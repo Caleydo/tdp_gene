49ee21da028e8644b016327ae23fe0cc2effe493
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-07-22 10:13:57 +0200

    last update

--- dist/views/ViewUtils.d.ts
+++ dist/views/ViewUtils.d.ts
@@ -1,9 +1,9 @@
 import { scale as d3Scale } from 'd3';
 export declare class ViewUtils {
-    static base: any;
-    static removed: any;
-    static colors: any;
+    static base: string[];
+    static removed: string[];
+    static colors: string[];
     static integrateColors(scale: d3Scale.Ordinal<string, string>, colors: string[]): void;
-    static colorScale(): any;
+    static colorScale(): d3Scale.Ordinal<string, string>;
     static legend(legend: HTMLElement, scale: d3Scale.Ordinal<string, string>): void;
 }
