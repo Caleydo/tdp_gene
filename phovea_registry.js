3974e997fec3960fad3be02c09e632f9ef16ace8
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-08-13 14:37:16 +0200

    prepare release 7.0.1

--- phovea_registry.js
+++ phovea_registry.js
@@ -4,7 +4,7 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 import {PluginRegistry} from 'phovea_core';
-import reg from './src/phovea';
+import reg from './dist/phovea';
 /**
  * build a registry by registering all phovea modules
  */
