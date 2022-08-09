2d0c134cc971aa1ba911f533f54c6e9307812fd7
anita-steiner <>
2022-08-09 12:19:26 +0000

    Prepare visyn_script changes

--- dist/views/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './ViewUtils';
-export * from './ACoExpression';
-export * from './ACommonList';
-export * from './AExpressionVsCopyNumber';
-export * from './AOncoPrint';
-export * from './GeneProxyView';
-export * from './UniProtProxyView';
-//# sourceMappingURL=index.d.ts.map
